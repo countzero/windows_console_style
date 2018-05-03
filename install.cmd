2fdbeda540f5496cd66a316a06ea1848076bb974
Finn Kumkar <finn.kumkar@gmail.com>
2018-05-03 10:10:37 +0200

    Add Screenshots

--- install.cmd
+++ install.cmd
@@ -1,10 +1,6 @@
 @echo off
 
-::
-:: Execute the main PowerShell script in the same window,
-:: bypass the Execution Policy and keep the window
-:: open after the script has finished.
-::
+:: Execute the main PowerShell script in the same window, bypassing the Execution Policy.
 PowerShell -NoProfile ^
            -ExecutionPolicy Bypass ^
            -File "%~dp0script\style_all_consoles.ps1"
